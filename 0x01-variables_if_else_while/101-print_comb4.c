c11c3182a53e87aece33f5c4ebb00cdddab1157d
Mohamed Reda <m07hamedreda25@gmail.com>
2023-06-16 12:45:29 -0700

    101--4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,13 +13,13 @@ int main(void)
 	int digit1 = 0;
 	int digit2, digit3;
 
-	while (digit1 < 9)
+	while (digit1 <= 9)
 	{
 		digit2 = 0;
-		while (digit2 < 9)
+		while (digit2 <= 9)
 		{
 			digit3 = 0;
-			while (digit3 < 9)
+			while (digit3 <= 9)
 			{
 				if (digit1 != digit2 &&
 				    digit1 < digit2 &&
