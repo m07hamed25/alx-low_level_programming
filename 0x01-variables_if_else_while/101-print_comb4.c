efb935b29bf559b4db867f6c0493e4e48c467912
Mohamed Reda <m07hamedreda25@gmail.com>
2023-06-16 12:42:00 -0700

    101--3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,19 +21,22 @@ int main(void)
 			digit3 = 0;
 			while (digit3 < 9)
 			{
-				if (digit1 != digit2 && digit1 < digit2 && digit2 != digit3 && digit2 < digit3)
-					{
-						putchar(digit1 + 48);
-						putchar(digit2 + 48);
-						putchar(digit3 + 48);
+				if (digit1 != digit2 &&
+				    digit1 < digit2 &&
+				    digit2 != digit3 &&
+				    digit2 < digit3)
+				{
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
 
-						if (digit1 + digit2 + digit3 != 24)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					if (digit1 + digit2 + digit3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
 					}
-					digit3++;
+				}
+				digit3++;
 			}
 			digit2++;
 		}
