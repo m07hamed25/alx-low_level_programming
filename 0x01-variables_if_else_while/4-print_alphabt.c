bd4153259737ad9d4b7cc15f5f0cf89abc49ab3b
Mohamed Reda <m07hamedreda25@gmail.com>
2023-06-16 11:40:20 -0700

    4--

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Description: print all aplhabet letters
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
