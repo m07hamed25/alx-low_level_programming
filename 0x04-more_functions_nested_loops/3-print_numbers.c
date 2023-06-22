3ba133041ff423ae7c01375d24020e793da1b558
Mohamed Reda <m07hamedreda25@gmail.com>
2023-06-22 07:19:31 -0700

    3--test

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * print_numbers - print the numbers since 0 up to 9
@@ -13,5 +13,5 @@ void print_numbers(void)
 	{
 		_putchar(x + '0');
 	}
-	_putchar('\n'0);
+	_putchar('\n');
 }
