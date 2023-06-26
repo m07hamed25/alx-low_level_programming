197f3d3ebc7d3943687e7b50749fdb0c14e901a6
Mohamed Reda <m07hamedreda25@gmail.com>
2023-06-26 14:26:05 -0700

    dsfdghjh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /*
- * print_alphabet_x10 - MAke alphabet x10 times
+ * print_alphabet_x10 - function that will print the alphabet 10 times
+ * Description: print all aplhabet letter.
  * Return: void
  */
 
