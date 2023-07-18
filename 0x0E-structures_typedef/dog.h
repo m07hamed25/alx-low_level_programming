e8e252cc46887a5c4d405488ad09a2c519a7161a
Mohamed Reda <m07hamedreda25@gmail.com>
2023-07-18 04:46:34 -0700

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @n: dog name
+ * @a:dog age
+ * @o: owner name
+ */
+
+struct dog
+{
+	char *n;
+	float a;
+	char *o;
+};
+
+#endif
