cb9e8042442deddbcf8ffe60c327abb2de90f633
Mohamed Reda <m07hamedreda25@gmail.com>
2023-07-19 04:30:58 -0700

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integer
+ * @a: yhe frist int
+ * @b: the second int
+ * Return: the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - adds two integer
+ * @a: yhe frist int
+ * @b: the second int
+ * Return: the sum
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - adds two integer
+ * @a: yhe frist int
+ * @b: the second int
+ * Return: the sum
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - adds two integer
+ * @a: yhe frist int
+ * @b: the second int
+ * Return: the sum
+ */
+
+int op_mod(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - adds two integer
+ * @a: yhe frist int
+ * @b: the second int
+ * Return: the sum
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
