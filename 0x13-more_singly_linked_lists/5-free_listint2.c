ed69e17d07430719616f09a301217a6abaa52bcc
mohamed reda <m07hamedreda25@gmail.com>
2023-07-31 17:00:32 -0700

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ * @head: address of pointer
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *temp;
+
+	if (!head)
+		return;
+
+	node = *head;
+	while (node)
+	{
+		temp = node;
+		node = node->next;
+		free(temp);
+	}
+	*head = NULL;
+}
