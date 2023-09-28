f56a53fa5f84e5e5d6b2c0cc1c76ebfe17f0790f
Mohamed Reda <m07hamedreda25@gmail.com>
2023-09-28 12:01:27 +0300

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _strlen_recursion - Calculate the length of string
+ * @s: the string to count
+ * Return: Integer value
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
