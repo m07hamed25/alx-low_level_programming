6075a7dd2e4408539d8da8fb4c6ded31c6e6f7dd
Mohamed Reda <m07hamedreda25@gmail.com>
2023-09-28 11:42:30 +0300

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,7 +9,10 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		return;
 	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
